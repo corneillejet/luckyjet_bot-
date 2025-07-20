fa4d61a72648f1e959c58711537f40d7866f6dfe
corneillejet <oulaicorneille9@gmail.com>
2025-07-20 15:39:41 +0000

    Ajout du fichier config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Configuration de base pour ton bot Lucky Jet
+
+# Ton token Telegram (remplace par ton vrai token)
+API_TOKEN = "7686696103:AAHpwJXtJH_3H593Sda_2YQ2MzN3r-PBfSs"
+
+# ID de ton canal ou groupe (si tu veux envoyer des messages automatiquement)
+CHANNEL_ID = "@skyprnosvip"
+
+# Optionnel : fuseau horaire
+TIMEZONE = "Africa/Abidjan"
